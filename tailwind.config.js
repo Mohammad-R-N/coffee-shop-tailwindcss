254d05ff19d9a30cdd4442272777eca3d4e025e7
mohammad-r-n <mohammad.ca@proton.me>
2024-04-30 01:52:30 -0400

    chore: custom tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./public/templates/*.{html,js}",
+    "./public/scripts/*.{html,js}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brown": {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832"
+        }
+      },
+      boxShadow: {
+        "normal": "0px 1px 10px rgba(0,0,0,0.05)",
+      },
+      borderRadius: {
+        "4xl": "2rem", //32px
+      },
+      fontFamily: {
+        "Dana": "Dana",
+        "DanaMedium": "Dana Medium",
+        "DanaDemiBold": "Dana DemiBold",
+        "Morabba": "Morabba Light",
+        "MorabbaMedium": "Morabba Medium",
+        "MorabbaBold": "Morabba Bold",
+      }
+    },
   },
   plugins: [],
 }
